a60784ee07523ffec6b19e322f84411d148e0716
HasanBakar <abubakar.cse@gmail.com>
2022-08-18 02:42:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById('btn_button').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'black';
+    const textColor = document.getElementById('text_color');
+    textColor.style.color = 'white';
+});
